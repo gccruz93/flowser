f66c1524f2902fb0b3cce11a7f60089026b8d710
Guilherme Cruz <gccruz93@gmail.com>
2024-01-02 12:33:34 -0300

    build dividida em raw e zip

--- build.js
+++ build.js
@@ -1,24 +1,29 @@
 import { cpSync, createWriteStream, mkdirSync, rmSync } from 'node:fs';
 import archiver from 'archiver';
+import packageJson from './package.json' assert { type: 'json' };
+
+console.log(`Building v${packageJson.version}`);
 
 /**
  * Copying files...
  */
-rmSync('./dist', { recursive: true });
-mkdirSync('./dist');
+try {
+  rmSync('./dist', { recursive: true });
+} catch (error) {}
+mkdirSync('./dist/raw', { recursive: true });
 
-cpSync('./manifest.json', './dist/manifest.json');
-cpSync('./service_worker.js', './dist/service_worker.js');
-cpSync('./popup', './dist/popup', { recursive: true });
-cpSync('./sites', './dist/sites', { recursive: true });
+cpSync('./manifest.json', './dist/raw/manifest.json');
+cpSync('./service_worker.js', './dist/raw/service_worker.js');
+cpSync('./popup', './dist/raw/popup', { recursive: true });
+cpSync('./sites', './dist/raw/sites', { recursive: true });
 
 /**
  * Zipping...
  */
-const output = createWriteStream('./dist.zip');
 const archive = archiver('zip');
+const output = createWriteStream(`./dist/flowser-${packageJson.version}.zip`);
 
-output.on('close', function () {
+output.on('close', () => {
   console.log(archive.pointer() + ' total bytes');
 });
 
@@ -26,6 +31,6 @@ archive.on('error', function (err) {
   throw err;
 });
 
-archive.directory('./dist', false);
+archive.directory('./dist/raw', false);
 archive.pipe(output);
 archive.finalize();
