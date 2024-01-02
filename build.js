772726c281da991ee9aaad511907992fb85fdd53
Guilherme Cruz <gccruz93@gmail.com>
2024-01-02 11:38:00 -0300

    feat: build step

---
+++ build.js
@@ -0,0 +1,9 @@
+import { cpSync, mkdirSync, rmSync } from 'node:fs';
+
+rmSync('./dist', {recursive: true})
+mkdirSync('./dist')
+
+cpSync('./manifest.json', './dist/manifest.json')
+cpSync('./service_worker.js', './dist/service_worker.js')
+cpSync('./popup', './dist/popup', { recursive: true })
+cpSync('./sites', './dist/sites', { recursive: true })
