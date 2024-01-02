56d4077b9345d193304db82ed511036ddb83219e
Guilherme Cruz <gccruz93@gmail.com>
2024-01-02 12:10:38 -0300

    build step com zip

--- build.js
+++ build.js
@@ -1,5 +1,9 @@
-import { cpSync, mkdirSync, rmSync } from 'node:fs';
+import { cpSync, createWriteStream, mkdirSync, rmSync } from 'node:fs';
+import archiver from 'archiver';
 
+/**
+ * Copying files...
+ */
 rmSync('./dist', { recursive: true });
 mkdirSync('./dist');
 
@@ -7,3 +11,21 @@ cpSync('./manifest.json', './dist/manifest.json');
 cpSync('./service_worker.js', './dist/service_worker.js');
 cpSync('./popup', './dist/popup', { recursive: true });
 cpSync('./sites', './dist/sites', { recursive: true });
+
+/**
+ * Zipping...
+ */
+const output = createWriteStream('./dist.zip');
+const archive = archiver('zip');
+
+output.on('close', function () {
+  console.log(archive.pointer() + ' total bytes');
+});
+
+archive.on('error', function (err) {
+  throw err;
+});
+
+archive.directory('./dist', false);
+archive.pipe(output);
+archive.finalize();
