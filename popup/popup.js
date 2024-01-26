ec2d3f15a5224c118e4760d9862e411829c0a4af
Guilherme Cruz <gccruz93@gmail.com>
2024-01-25 22:44:28 -0300

    using corejs + storage fixed

--- popup/popup.js
+++ popup/popup.js
@@ -1,34 +1,19 @@
 const init = async () => {
-  let defaultStorage = {
-    config: {
-      logs: false,
-    },
-    sites: {
-      youtube: {
-        autoConfirmSkip: true,
-        autoConfirmSkipCount: 0,
-        autoVideoAdSkip: true,
-        autoVideoAdSkipCount: 0,
-        blockAdsCards: true,
-      },
-      twitch: {
-        autoAdsMute: true,
-        autoAdsMuteCount: 0,
-      },
-    },
-  };
+  await flowser.storage.init();
 
-  const loadStorage = async () => {
-    if (!chrome.storage) return;
-    const storage = await chrome.storage.sync.get(['sites', 'config']);
-    defaultStorage = storage;
-  };
-  const saveStorage = () => {
-    if (!chrome.storage) return;
-    chrome.storage.sync.set(defaultStorage);
-  };
-  loadStorage();
-  console.log('storage', defaultStorage);
+  setInterval(() => {
+    $('#config-chk-logs').prop('checked', flowser.storage.data.config.logs);
+
+    $('#youtube-chk-confirmSkip').prop(
+      'checked',
+      flowser.storage.data.sites.youtube.confirmSkip
+    );
+
+    $('#twitch-chk-adsMute').prop(
+      'checked',
+      flowser.storage.data.sites.twitch.adsMute
+    );
+  }, 1000);
 
   /**
    * Config
@@ -47,18 +32,18 @@ const init = async () => {
     }
   };
   $('#config-btn').on('click', toggleConfig);
-  $('#config-chk-logs').prop('checked', defaultStorage.config.logs);
+  $('#config-chk-logs').prop('checked', flowser.storage.data.config.logs);
   $('#config-chk-logs').on('change', (e) => {
-    defaultStorage.config.logs = e.target.checked;
-    saveStorage();
+    flowser.storage.data.config.logs = e.target.checked;
+    flowser.storage.save();
   });
 
   /**
    * Sites.Youtube
    */
   $('#youtube-total-count').text(
-    defaultStorage.sites.youtube.autoConfirmSkipCount +
-      defaultStorage.sites.youtube.autoVideoAdSkipCount
+    flowser.storage.data.sites.youtube.confirmSkipCount +
+      flowser.storage.data.sites.youtube.videoAdSkipCount
   );
   $('#youtube-collapse').hide();
   let isYoutubeOpened = false;
@@ -75,37 +60,19 @@ const init = async () => {
   };
   $('#youtube-collapse-title').on('click', toggleYoutube);
 
-  $('#youtube-chk-autoVideoAdSkip').prop(
+  $('#youtube-chk-confirmSkip').prop(
     'checked',
-    defaultStorage.sites.youtube.autoVideoAdSkip
+    flowser.storage.data.sites.youtube.confirmSkip
   );
-  $('#youtube-chk-autoVideoAdSkip').on('change', (e) => {
-    defaultStorage.sites.youtube.autoVideoAdSkip = e.target.checked;
-    saveStorage();
-  });
-
-  $('#youtube-chk-autoConfirmSkip').prop(
-    'checked',
-    defaultStorage.sites.youtube.autoConfirmSkip
-  );
-  $('#youtube-chk-autoConfirmSkip').on('change', (e) => {
-    defaultStorage.sites.youtube.autoConfirmSkip = e.target.checked;
-    saveStorage();
-  });
-
-  $('#youtube-chk-blockAdsCards').prop(
-    'checked',
-    defaultStorage.sites.youtube.blockAdsCards
-  );
-  $('#youtube-chk-blockAdsCards').on('change', (e) => {
-    defaultStorage.sites.youtube.blockAdsCards = e.target.checked;
-    saveStorage();
+  $('#youtube-chk-confirmSkip').on('change', (e) => {
+    flowser.storage.data.sites.youtube.confirmSkip = e.target.checked;
+    flowser.storage.save();
   });
 
   /**
    * Sites.Twitch
    */
-  $('#twitch-total-count').text(defaultStorage.sites.twitch.autoAdsMuteCount);
+  $('#twitch-total-count').text(flowser.storage.data.sites.twitch.adsMuteCount);
   $('#twitch-collapse').hide();
   let isTwitchOpened = false;
   const toggleTwitch = () => {
@@ -121,29 +88,14 @@ const init = async () => {
   };
   $('#twitch-collapse-title').on('click', toggleTwitch);
 
-  $('#twitch-chk-autoAdsMute').prop(
+  $('#twitch-chk-adsMute').prop(
     'checked',
-    defaultStorage.sites.twitch.autoAdsMute
+    flowser.storage.data.sites.twitch.adsMute
   );
-  $('#twitch-chk-autoAdsMute').on('change', (e) => {
-    defaultStorage.sites.twitch.autoAdsMute = e.target.checked;
-    saveStorage();
+  $('#twitch-chk-adsMute').on('change', (e) => {
+    flowser.storage.data.sites.twitch.adsMute = e.target.checked;
+    flowser.storage.save();
   });
 };
 
-document.addEventListener('readystatechange', (event) => {
-  if (event.target.readyState === 'interactive') {
-    init();
-  } else if (event.target.readyState === 'complete') {
-    init();
-  }
-});
-
-function mergeObjects(obj1, obj2) {
-  for (let key in obj1) {
-    if (obj2.hasOwnProperty(key)) {
-      obj1[key] = obj2[key];
-    }
-  }
-  return obj1;
-}
+init();
